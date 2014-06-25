e85d683c6318a4b460c1846118d66225da9e438d
leiyonglin <leiyonglin@gmail.com>
2014-06-25 23:43:50 +0800

    update

--- trie/trie.go
+++ trie/trie.go
@@ -1,14 +1,67 @@
 package trie
 
-import ()
-
 type Trie struct {
-	count    int
-	children []*Trie
+	Value    byte
+	Count    int
+	End      int
+	Children map[byte]*Trie
 }
 
-func insert() {
+func New() *Trie {
+	return &Trie{
+		Children: make(map[byte]*Trie),
+	}
 }
 
-func get() {
+func (t *Trie) Insert(w []byte) error {
+	l := len(w)
+	if l == 0 {
+		return nil
+	}
+
+	for _, v := range w {
+		t = t.insert(v)
+	}
+	t.last()
+
+	return nil
+}
+
+func (t *Trie) insert(w byte) *Trie {
+	if _, ok := t.Children[w]; !ok {
+		t.Children[w] = New()
+		t.Children[w].Value = w
+	}
+
+	t.Count++
+	return t.Children[w]
+}
+
+func (t *Trie) Get(w []byte) (*Trie, bool) {
+	var ok bool
+
+	for _, v := range w {
+		t, ok = t.get(v)
+		if !ok {
+			return t, false
+		}
+	}
+
+	if t.End == 0 {
+		return t, false
+	}
+
+	return t, true
+}
+
+func (t *Trie) get(w byte) (*Trie, bool) {
+	if _, ok := t.Children[w]; ok {
+		return t.Children[w], true
+	}
+
+	return t, false
+}
+
+func (t *Trie) last() {
+	t.End++
 }
